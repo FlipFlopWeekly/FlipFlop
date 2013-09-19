de31ed72348a0bd1e27ccb2b480dd3868c1109ac
Linzo <ndiayelinzo@gmail.com>
2013-09-19 22:28:07 +0200

    [Test]1st commit

--- src/FlipFlop/SiteBundle/Controller/DefaultController.php
+++ src/FlipFlop/SiteBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	// First commit test
     public function indexAction($name)
     {
         return $this->render('FlipFlopSiteBundle:Default:index.html.twig', array('name' => $name));
