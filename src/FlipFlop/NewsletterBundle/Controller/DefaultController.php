81e78fc09b5c50b827c11598158c0e5a2622e9d4
Sébastien Correaud <seb.correaud@cgi.com>
2013-10-02 13:51:19 +0200

    Build 0.1.2 : Templating newsletter

--- src/FlipFlop/NewsletterBundle/Controller/DefaultController.php
+++ src/FlipFlop/NewsletterBundle/Controller/DefaultController.php
@@ -10,4 +10,12 @@ class DefaultController extends Controller
     {
         return $this->render('FlipFlopNewsletterBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function templatingaction()
+    {
+        $parameters = array();
+        $parameters['crew_name'] = $this->container->parameters['crew_name'];
+
+        return $this->render('FlipFlopNewsletterBundle:Default:template.html.twig', array('parameters' => $parameters));
+    }
 }
