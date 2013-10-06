64de91fd61cdc53951c0c8c93b31c9a158c03214
xymox18 <xymox18@vLucybell>
2013-10-06 20:14:24 +0200

    Carousel Home Page

--- src/FlipFlop/SiteBundle/Resources/public/js/flipflop.js
+++ src/FlipFlop/SiteBundle/Resources/public/js/flipflop.js
@@ -5,6 +5,17 @@
 $(document).ready(function() {
     $('#slideshowHolder').jqFancyTransitions({ 
         width: 975,
-        height: 300
+        height: 300,
+        effect: 'zipper', // wave, zipper, curtain
+        strips: 20, // number of strips
+        delay: 5000, // delay between images in ms
+        stripDelay: 50, // delay beetwen strips in ms
+        titleOpacity: 0.7, // opacity of title
+        titleSpeed: 1000, // speed of title appereance in ms
+        position: 'alternate', // top, bottom, alternate, curtain
+        direction: 'fountainAlternate', // left, right, alternate, random, fountain, fountainAlternate
+        navigation: true, // prev and next navigation buttons
+        links: true // show images as links
+
     });
 });
