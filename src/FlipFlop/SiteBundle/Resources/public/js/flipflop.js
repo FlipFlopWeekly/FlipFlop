b424a7cb3fb191e62e289d843f5dfc14801a0a04
xymox18 <xymox18@vLucybell>
2013-10-06 19:33:05 +0200

    Carousel Home Page

---
+++ src/FlipFlop/SiteBundle/Resources/public/js/flipflop.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function() {
+    $('#slideshowHolder').jqFancyTransitions({ 
+        width: 975,
+        height: 300
+    });
+});
