1f161dc9d6b71fcedc65aacddbca5a0a7eeb2eaf
iGitScor <sebastien.correaud@gmail.com>
2013-10-15 08:40:30 +0200

    Create service : newsletter

--- src/FlipFlop/NewsletterBundle/Controller/DefaultController.php
+++ src/FlipFlop/NewsletterBundle/Controller/DefaultController.php
@@ -6,19 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * Auto generated indexAction.
+     *
+     * @param unknown_type $name
+     */
     public function indexAction($name)
     {
-        return $this->render('FlipFlopNewsletterBundle:Default:index.html.twig', array('name' => $name));
-    }
-
-    /**
-     * Static template of the newsletter.
-     */
-    public function templatingaction()
-    {
-        $parameters = array();
-        $parameters['crew_name'] = $this->container->parameters['crew_name'];
-
-        return $this->render('FlipFlopNewsletterBundle:Default:template.html.twig', array('parameters' => $parameters));
+        return $this->render( 'FlipFlopNewsletterBundle:Default:index.html.twig', array( 'name' => $name ) );
     }
 }
